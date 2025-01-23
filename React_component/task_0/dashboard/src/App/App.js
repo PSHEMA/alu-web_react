5b826ba64b0ce79066759d3b22ba99d9e00c3df4
PSHEMA <p.shema@alustudent.com>
2025-01-23 20:50:22 +0200

    App.js

---
+++ React_component/task_0/dashboard/src/App/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import React from 'react';
+import { hot } from 'react-hot-loader';
+
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import { Notifications } from '../Notifications/Notifications';
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Notifications />
+        <div className="App">
+          <Header />
+          <Login />
+          <Footer />
+        </div>
+      </>
+    );
+  }
+}
+
+export default hot(module)(App);
